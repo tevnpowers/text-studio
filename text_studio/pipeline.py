b187422ef514d28a07cd0fe0a48bcf29e0d6762e
Tev'n Powers <tevnpowers@gmail.com>
2020-02-14 12:17:55 -0800

    update pipeline to reflect more generalized components (actions and modules)

--- text_studio/pipeline.py
+++ text_studio/pipeline.py
@@ -1,21 +1,27 @@
 from collections import OrderedDict
 
+from text_studio.action import Action
+from text_studio.transformer import Transformer
+
 
 class Pipeline(object):
-    def __init__(self, id, name="", modules=None):
+    def __init__(self, id, name="", components=None):
         self.id = id
         self.name = name
-        self.modules = modules if modules else OrderedDict()
+        self.components = components if components else OrderedDict()
 
-    def add_module(self, module):
-        self.modules[module.id] = module
+    def add_component(self, component):
+        self.components[component.id] = component
 
-    def remove_module(self, id):
-        del self.modules[id]
+    def remove_component(self, id):
+        del self.components[id]
 
     def execute(self, data):
         """Process and return a single text document."""
         processed_data = data.instances
-        for module in self.modules:
-            processed_data = module.process_batch(processed_data)
+        for component in self.components:
+            if isinstance(component, Transformer):
+                processed_data = component.process_batch(processed_data)
+            elif isinstance(component, Action):
+                component.process_batch(processed_data)
         data.instances = processed_data
