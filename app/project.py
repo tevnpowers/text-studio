a44810d89dbc934dc37c888a840d96d67076087d
Tev'n Powers <tevnpowers@gmail.com>
2020-02-14 12:18:31 -0800

    add support for Action components in projects

--- app/project.py
+++ app/project.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from uuid import UUID, uuid4
 from extensions.html_parser import HtmlParser
 from extensions.accumulator import Accumulator
+from extensions.word_cloud import WordCloudGenerator
 
 from text_studio.dataset import Dataset
 from text_studio.pipeline import Pipeline
@@ -29,6 +30,7 @@ class Project(object):
 
         self.datasets = {}
         self.modules = {}
+        self.actions = {}
         self.pipelines = {}
 
         if filepath:
@@ -49,6 +51,8 @@ class Project(object):
             self.metadata["created"],
             self.metadata["saved"],
         )
+
+        # TO DO: Add output info for datasets, modules, actions & pipelines.
         return description
 
     @property
@@ -74,6 +78,10 @@ class Project(object):
             for module in config["modules"]:
                 self.add_module(module)
 
+        if "actions" in config:
+            for action in config["actions"]:
+                self.add_action(action)
+
         if "pipelines" in config:
             for info in config["pipelines"]:
                 self.add_pipeline(info)
@@ -82,24 +90,38 @@ class Project(object):
         id = UUID(info["id"])
         self.datasets[id] = Dataset(id, self._get_absolute_path(info["path"]))
 
-    def add_module(self, module_info):
-        if module_info["name"] == "HtmlParser":
+    def add_module(self, info):
+        if info["name"] == "HtmlParser":
             module_class = HtmlParser
-        elif module_info["name"] == "Accumulator":
+        elif info["name"] == "Accumulator":
             module_class = Accumulator
 
-        id = UUID(module_info["id"])
-        module_info["config"]["id"] = id
-        kwargs = module_info["config"]
+        id = UUID(info["id"])
+        info["config"]["id"] = id
+        kwargs = info["config"]
         module = module_class()
         module.setup(**kwargs)
         self.modules[id] = module
 
+    def add_action(self, info):
+        if info["name"] == "WordCloud":
+            action_class = WordCloudGenerator
+
+        id = UUID(info["id"])
+        info["config"]["id"] = id
+        kwargs = info["config"]
+        action = action_class()
+        action.setup(**kwargs)
+        self.actions[id] = action
+
     def add_pipeline(self, info):
         pipeline = Pipeline(UUID(info["id"]), info["name"])
-        for id in info["modules"]:
+        for id in info["components"]:
             id = UUID(id)
-            pipeline.add_module(self.modules[id])
+            if id in self.modules:
+                pipeline.add_component(self.modules[id])
+            elif id in self.actions:
+                pipeline.add_component(self.actions[id])
         self.pipelines[pipeline.id] = pipeline
 
     def load_datasets(self):
@@ -110,14 +132,16 @@ class Project(object):
         instances = self.datasets[input_data_id].instances[:1000]
         if id in self.modules:
             instances = self._run_module(id, instances, verbose)
+        elif id in self.actions:
+            self._run_action(id, instances, output_data_path, verbose)
         elif id in self.pipelines:
-            instances = self._run_pipeline(id, instances, verbose)
+            instances = self._run_pipeline(id, instances, output_data_path, verbose)
         else:
             raise KeyError(
                 "The provided ID does not exist in project modules or pipelines."
             )
 
-        if output_data_path:
+        if output_data_path and id not in self.actions:
             absolute_path = self._get_absolute_path(output_data_path)
             matching_id = None
             for id, dataset in self.datasets.items():
@@ -143,10 +167,21 @@ class Project(object):
             output.append(result)
         return output
 
-    def _run_pipeline(self, id, data, verbose=False):
+    def _run_action(self, id, data, output_path, verbose=False):
+        action = self.actions[id]
+        if verbose:
+            print("Executing action {}...".format(action.name))
+
+        absolute_path = self._get_absolute_path(output_path)
+        action.process_batch(data, absolute_path)
+
+    def _run_pipeline(self, id, data, output_path, verbose=False):
         print("Executing pipeline {}...".format(self.pipelines[id].name))
-        for id in self.pipelines[id].modules:
-            data = self._run_module(id, data, verbose)
+        for id in self.pipelines[id].components:
+            if id in self.modules:
+                data = self._run_module(id, data, verbose)
+            elif id in self.actions:
+                self._run_action(id, data, output_path)
         return data
 
     def _get_absolute_path(self, path):
