d46bdf28c9627264dfc701b5b3fe407d1534f093
Tev'n Powers <tevnpowers@gmail.com>
2020-03-09 00:44:42 -0700

    adding project and download URLS for package release

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 import setuptools
 
+PROJECT_URLS = {
+    "Bug Tracker": "https://github.com/tevnpowers/text-studio/issues",
+    "Documentation": "https://github.com/tevnpowers/text-studio/tree/master/docs",
+    "Source Code": "https://github.com/tevnpowers/text-studio",
+}
+
+DOWNLOAD_URL = "https://pypi.org/project/text-studio/#files"
+
 with open("text_studio/README.md", "r") as fh:
     long_description = fh.read()
 
@@ -13,8 +21,10 @@ setuptools.setup(
     description=short_description,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    download_url=DOWNLOAD_URL,
+    project_urls=PROJECT_URLS,
     url="https://github.com/tevnpowers/text-studio",
-    packages=['text_studio'],
+    packages=["text_studio"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
